835f5a3c05d7dde797af505bfe5a998db6a248f3
UnburnedAlliance <linenotsecure@gmail.com>
2024-07-16 00:34:25 -0500

    Reverted webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,26 +2,26 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const fs = require('fs');
 
-// Function to generate HtmlWebpackPlugin instances for each HTML file in a directory
-function generateHtmlPlugins(templateDir) {
-  const templateFiles = fs.readdirSync(path.resolve(__dirname, templateDir)).filter(file => file.endsWith('.html'));
+// Function to generate HtmlWebpackPlugin instances for each HTML file in the src folder
+function generateHtmlPlugins() {
+  const htmlFiles = fs.readdirSync(path.resolve(__dirname, 'src')).filter(file => file.endsWith('.html'));
 
-  return templateFiles.map(file => {
+  return htmlFiles.map(file => {
     return new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, templateDir, file),
-      filename: path.join(templateDir.replace('src/', ''), file)
+      template: `./src/${file}`,
+      filename: file,
     });
   });
 }
 
 module.exports = {
   entry: {
-    main: './src/js/index.js',
+    main: './src/js/index.js', 
     styles: './src/css/index.js'
-  },
+  }, // Entry point is main.js in src/js directory
   output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
   },
   devServer: {
     static: path.resolve(__dirname, 'dist'),
@@ -31,23 +31,23 @@ module.exports = {
   resolve: {
     fallback: {
       "zlib": require.resolve('browserify-zlib'),
-      "crypto": require.resolve("crypto-browserify"),
-      "buffer": false,
-      "vm": require.resolve("vm-browserify"),
-      "stream": require.resolve("stream-browserify"),
-      "querystring": require.resolve("querystring-es3"),
-      "http": require.resolve("stream-http"),
+      "crypto": require.resolve("crypto-browserify"), 
+      "buffer": false, // was: require.resolve("buffer"), 
+      "vm": require.resolve("vm-browserify"), 
+      "stream": require.resolve("stream-browserify"), 
+      "querystring": require.resolve("querystring-es3"), 
+      "http": require.resolve("stream-http"), 
       "fs": false,
-      "async_hooks": false,
-      "browser": false,
+      "async_hooks": false, 
+      "browser": false, 
       "net": false
     }
-  },
+  },  
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        test: /\.css$/, // Match CSS files
+        use: ['style-loader', 'css-loader'], // Use style-loader and css-loader
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
@@ -55,17 +55,17 @@ module.exports = {
           loader: 'file-loader',
           options: {
             name: '[path][name].[ext]',
-            context: 'src',
-            outputPath: '/',
-            publicPath: '/'
+            context: 'src',  // Keeps the original folder structure
+            outputPath: '/',  // Outputs images to dist/assets/ // was: assets/
+            publicPath: '/'  // Ensures images are served from the correct path
           }
         }
       }
     ]
   },
   plugins: [
-    ...generateHtmlPlugins('src'),
-    ...generateHtmlPlugins('src/creators')
+    // Generate HtmlWebpackPlugin instances dynamically for each HTML file in the src folder
+    ...generateHtmlPlugins()
   ],
   mode: 'production'
 };
