ea4e5448b63a1ff33556fd71c8cef12fceb2b14b
UnburnedAlliance <linenotsecure@gmail.com>
2024-07-15 22:56:41 -0500

    working on wrong single-page setup

--- server.js
+++ server.js
@@ -6,10 +6,12 @@ const PORT = process.env.PORT || 3000;
 // Serve static files from the 'dist' directory
 app.use(express.static(path.join(__dirname, 'dist')));
 
+/* testing this fucking things up
 // Fallback to index.html for single-page applications
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
+*/
 
 // Start the server
 app.listen(PORT, () => {
